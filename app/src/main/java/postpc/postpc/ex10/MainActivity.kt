b4f41fd111038d55444656570f912548aadf6f21
Eli Yacobov <eli.yacobov1@gmail.com>
2021-07-03 11:43:27 +0300

    done. User cannot enter empty user-name. Empty pretty name will cause default user-name to be rendered.

--- app/src/main/java/postpc/postpc/ex10/MainActivity.kt
+++ app/src/main/java/postpc/postpc/ex10/MainActivity.kt
@@ -26,11 +26,11 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var welcomeText: TextView
     private lateinit var updatePrettyName: Button
-    private lateinit var prettyNameText: TextView
+    private lateinit var prettyNameInput: TextView
     private lateinit var userImage: ImageView
-    private var savedToken: String? = null
+    private var currToken: String? = null
     private lateinit var connectButton: Button
-    private lateinit var usernameInput: EditText
+    private lateinit var userNameInput: EditText
     private var sp: SharedPreferences? = null
 
     /**
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             )
             .setInputData(
                 Data.Builder()
-                    .putString("token", savedToken)
+                    .putString("token", currToken)
                     .putString("image_url", url)
                     .putString("pretty_name", prettyName).build()
             )
@@ -78,8 +78,60 @@ class MainActivity : AppCompatActivity() {
                     val userResponse: UserResponse =
                         Gson().fromJson(userResponseAsJson, UserResponse::class.java)
                     val user: User = userResponse.data
-                    toggleLoadingStatus(false)
-                    showUserInfo(user)
+                    toggleLoading(false)
+                    renderUserInfo(user)
+                })
+    }
+
+    /**
+     * this methods sends get requests for users and tokens
+     */
+    private fun getUserOrTokenRequest(username: String?=null) {
+        if(username == ""){
+            Toast.makeText(applicationContext, "Please enter a valid username!", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }
+        if(username == null){
+            userNameInput.visibility = View.INVISIBLE
+            connectButton.visibility = View.INVISIBLE
+            toggleLoading(true)
+        }
+        val id = UUID.randomUUID()
+        val workerType =  if (username == null) GetUserWorker::class.java else GetTokenWorker::class.java
+        val getRequestWork = OneTimeWorkRequest.Builder(workerType)
+            .setConstraints(
+                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+            )
+            .setInputData(Data.Builder()
+                .putString("user_name", username)
+                .putString("token", currToken)
+                .build())
+            .addTag(id.toString())
+            .build()
+        WorkManager.getInstance(applicationContext).enqueue(getRequestWork)
+        WorkManager.getInstance(applicationContext)
+            .getWorkInfosByTagLiveData(id.toString()).observe(this,
+                Observer {
+                    if (it == null || it.isEmpty()) return@Observer
+                    val res = it[0].outputData.getString("output")
+                    if (res == null || res == "") {
+                        return@Observer
+                    }
+                    if(username == null){
+                        val userResponse: UserResponse = Gson().fromJson(res, UserResponse::class.java)
+                        val user: User = userResponse.data
+                        toggleLoading(false)
+                        renderUserInfo(user)
+                    }
+                    else {
+                        val token: TokenResponse = Gson().fromJson(res, TokenResponse::class.java)
+                        if (token.data == "") {
+                            return@Observer
+                        }
+                        saveToken(token.data)
+                        getUserOrTokenRequest()
+                    }
                 })
     }
 
@@ -88,35 +140,47 @@ class MainActivity : AppCompatActivity() {
      */
     private fun loadImages() {
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_ALIEN)
-            .into(findViewById<View>(R.id.image_alien) as ImageView)
+            .into(findViewById<View>(R.id.alien_img) as ImageView)
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_CRAB)
-            .into(findViewById<View>(R.id.image_crab) as ImageView)
+            .into(findViewById<View>(R.id.crab_img) as ImageView)
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_FROG)
-            .into(findViewById<View>(R.id.image_frog) as ImageView)
+            .into(findViewById<View>(R.id.frog_img) as ImageView)
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_OCTOPUS)
-            .into(findViewById<View>(R.id.image_octopus) as ImageView)
+            .into(findViewById<View>(R.id.octopus_img) as ImageView)
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_ROBOT)
-            .into(findViewById<View>(R.id.image_robot) as ImageView)
+            .into(findViewById<View>(R.id.robot_img) as ImageView)
         Glide.with(this).load(Paths.BASE_URL + Paths.IMG_UNICORN)
-            .into(findViewById<View>(R.id.image_unicorn) as ImageView)
-        findViewById<View>(R.id.image_alien).setOnClickListener {
+            .into(findViewById<View>(R.id.unicorn_img) as ImageView)
+        findViewById<View>(R.id.alien_img).setOnClickListener {
             updateUser(url=Paths.IMG_ALIEN)
         }
-        findViewById<View>(R.id.image_crab).setOnClickListener {
+        findViewById<View>(R.id.crab_img).setOnClickListener {
             updateUser(url=Paths.IMG_CRAB)
         }
-        findViewById<View>(R.id.image_frog).setOnClickListener {
+        findViewById<View>(R.id.frog_img).setOnClickListener {
             updateUser(url=Paths.IMG_FROG)
         }
-        findViewById<View>(R.id.image_octopus).setOnClickListener {
+        findViewById<View>(R.id.octopus_img).setOnClickListener {
             updateUser(url=Paths.IMG_OCTOPUS)
         }
-        findViewById<View>(R.id.image_robot).setOnClickListener {
+        findViewById<View>(R.id.robot_img).setOnClickListener {
             updateUser(url=Paths.IMG_ROBOT)
         }
-        findViewById<View>(R.id.image_unicorn).setOnClickListener {
+        findViewById<View>(R.id.unicorn_img).setOnClickListener {
             updateUser(url=Paths.IMG_UNICORN)
         }
+    }
+
+    private fun renderUserInfo(user: User) {
+        val name: String? = if (user.pretty_name == null || user.pretty_name.equals("")) user.username.toString() else user.pretty_name
+        welcomeText.text = String.format(Paths.WELCOME_MSG, name)
+        welcomeText.visibility = View.VISIBLE
+        updatePrettyName.visibility = View.VISIBLE
+        prettyNameInput.visibility = View.VISIBLE
+        Glide.with(this).load(Paths.BASE_URL + user.image_url).into(userImage)
+        Glide.with(this).load(Paths.BASE_URL + user.image_url).into(userImage)
+        userImage.visibility = View.VISIBLE
+        loadImages()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -124,118 +188,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //initialize UI
-        usernameInput = findViewById(R.id.username)
+        userNameInput = findViewById(R.id.username)
         connectButton = findViewById(R.id.connect)
-        prettyNameText = findViewById(R.id.new_name)
-        prettyNameText.visibility = View.INVISIBLE
+        prettyNameInput = findViewById(R.id.new_name)
+        prettyNameInput.visibility = View.INVISIBLE
         updatePrettyName = findViewById(R.id.change_name)
         updatePrettyName.visibility = View.INVISIBLE
         welcomeText = findViewById(R.id.welcome)
         welcomeText.visibility = View.INVISIBLE
-        userImage = findViewById(R.id.user_image)
+        userImage = findViewById(R.id.img)
         userImage.visibility = View.INVISIBLE
         requestPermission()
-//        sp = getPreferences(MODE_PRIVATE)
-//        savedToken = sp.getString(TOKEN, null)
-//        if (savedToken != null) {
-//            userInfo()
-//        }
 
-        /* define on click listeners */
-        connectButton.setOnClickListener { getUserToken(usernameInput.text.toString()) }
-        updatePrettyName.setOnClickListener { updateUser(prettyName=prettyNameText.text.toString()) }
-    }
-
-    private fun getUserToken(username: String) {
-        val id = UUID.randomUUID()
-        val checkConnectivityWork = OneTimeWorkRequest.Builder(GetTokenWorker::class.java)
-            .setConstraints(
-                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-            )
-            .setInputData(Data.Builder().putString("user_name", username).build())
-            .addTag(id.toString())
-            .build()
-        WorkManager.getInstance(applicationContext).enqueue(checkConnectivityWork)
-        WorkManager.getInstance(applicationContext)
-            .getWorkInfosByTagLiveData(id.toString()).observe(this,
-                Observer {
-                    if (it == null || it.isEmpty()) return@Observer
-                    val tokenAsJson = it[0].outputData.getString("output")
-                    if (tokenAsJson == null || tokenAsJson == "") {
-                        return@Observer
-                    }
-                    val token: TokenResponse = Gson().fromJson(tokenAsJson, TokenResponse::class.java)
-                    if (token.data == "") {
-                        return@Observer
-                    }
-                    saveToken(token.data)
-                    user()
-                })
-    }
-
-    private fun toggleLoadingStatus(show: Boolean) {
-        val load = findViewById<LinearLayout>(R.id.loading_progress)
-        if (show) {
-            load.visibility = View.VISIBLE
-        } else {
-            load.visibility = View.INVISIBLE
+        /* define on-click listeners */
+        connectButton.setOnClickListener {
+            userNameInput.clearFocus()
+            hideSoftKeyboard(userNameInput)
+            getUserOrTokenRequest(username=userNameInput.text.toString())
+        }
+        updatePrettyName.setOnClickListener {
+            prettyNameInput.clearFocus()
+            hideSoftKeyboard(prettyNameInput)
+            updateUser(prettyName=prettyNameInput.text.toString())
         }
     }
 
-    private fun user() {
-        usernameInput.visibility = View.INVISIBLE
-        connectButton.visibility = View.INVISIBLE
-        toggleLoadingStatus(true)
-        val id =  UUID.randomUUID()
-        val getUserWork = OneTimeWorkRequest.Builder(GetUserWorker::class.java)
-            .setConstraints(
-                Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
-            )
-            .setInputData(Data.Builder().putString("token", savedToken).build())
-            .addTag(id.toString())
-            .build()
-        WorkManager.getInstance(applicationContext).enqueue(getUserWork)
-        WorkManager.getInstance(applicationContext)
-            .getWorkInfosByTagLiveData(id.toString()).observe(this,
-                Observer {
-                    if (it == null || it.isEmpty()) return@Observer
-                    val userResponseAsJson = it[0].outputData.getString("output")
-                    if (userResponseAsJson == null || userResponseAsJson == "") {
-                        return@Observer
-                    }
-                    val userResponse: UserResponse = Gson().fromJson(userResponseAsJson, UserResponse::class.java)
-                    val user: User = userResponse.data
-                    toggleLoadingStatus(false)
-                    showUserInfo(user)
-                })
-    }
-
-    private fun showUserInfo(user: User) {
-        val name: String? = if (user.pretty_name == null || user.pretty_name.equals("")) user.username.toString() else user.pretty_name
-        welcomeText.text = String.format(Paths.WELCOME_MSG, name)
-        welcomeText.visibility = View.VISIBLE
-        updatePrettyName.visibility = View.VISIBLE
-        prettyNameText.visibility = View.VISIBLE
-        Glide.with(this).load(Paths.BASE_URL + user.image_url).into(userImage)
-        Glide.with(this).load(Paths.BASE_URL + user.image_url).into(userImage)
-        userImage.visibility = View.VISIBLE
-        loadImages()
+    private fun toggleLoading(isVisible: Boolean) {
+        val loadingElement = findViewById<LinearLayout>(R.id.loading)
+        loadingElement.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
     }
 
     private fun requestPermission() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.INTERNET
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET)
+            != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET), 100)
         }
     }
 
     private fun saveToken(token: String) {
-        savedToken = token
-//        val editor = sp.edit()
-//        editor.putString(TOKEN, token)
-//        editor.apply()
+        currToken = token
     }
 }
--- app/src/main/java/postpc/postpc/ex10/server/Paths.kt
+++ app/src/main/java/postpc/postpc/ex10/server/Paths.kt
@@ -3,13 +3,12 @@ package postpc.postpc.ex10.server
 class Paths {
     companion object {
         const val BASE_URL = "https://hujipostpc2019.pythonanywhere.com/"
-        const val WELCOME_MSG = "Welcome Back,\n %s!"
+        const val WELCOME_MSG = "Welcome,\n %s!"
         const val IMG_CRAB = "/images/crab.png"
         const val IMG_UNICORN = "/images/unicorn.png"
         const val IMG_ALIEN = "/images/alien.png"
         const val IMG_ROBOT = "/images/robot.png"
         const val IMG_OCTOPUS = "/images/octopus.png"
         const val IMG_FROG = "/images/frog.png"
-        const val TOKEN = "key_token"
     }
 }
